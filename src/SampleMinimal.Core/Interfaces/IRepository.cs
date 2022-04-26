e995de2cf52830b3d8f2c03252347d79f8de1db4
yun <yun@portj2.com>
2022-04-25 22:23:03 -0700

    Add generic repository

---
+++ src/SampleMinimal.Core/Interfaces/IRepository.cs
@@ -0,0 +1,16 @@
+﻿namespace SampleMinimal.Core.Interfaces
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        Task<List<T>> GetAllAsync();
+        Task<List<T>> QueryAsync(Expression<Func<T, bool>> filter);
+        Task<T> QuerySingleAsync(Expression<Func<T, bool>> filter);
+        Task<T> GetByIdAsync(int id);
+        Task<T> GetByGuidAsync(Guid guid);
+        Task<T> GetByNameAsync(string name);
+        Task<bool> AddAsync(T entity);
+        Task<bool> UpdateAsync(T entity);
+        Task<bool> DeleteAsync(T entity);
+        Task<bool> SaveAsync();
+    }
+}
