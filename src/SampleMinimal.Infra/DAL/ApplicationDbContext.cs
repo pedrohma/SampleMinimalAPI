94441e5c4eba11f9b694e5fade3ead878fa82a0e
yun <yun@portj2.com>
2022-04-25 22:23:14 -0700

    Add dbcontext

---
+++ src/SampleMinimal.Infra/DAL/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿namespace SampleMinimal.Infra.DAL
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Todo> Todos { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
