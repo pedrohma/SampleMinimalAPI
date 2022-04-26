f98e9b8e12bb14bbe90b09b5b630b58d085437da
yun <yun@portj2.com>
2022-04-25 22:22:26 -0700

    Add automapper profile

---
+++ src/SampleMinimal/MapProfiles/TodoProfile.cs
@@ -0,0 +1,10 @@
+﻿namespace SampleMinimal.API.MapProfiles
+{
+    public class TodoProfile : Profile
+    {
+        public TodoProfile()
+        {
+            CreateMap<Todo, TodoProfile>().ReverseMap();
+        }
+    }
+}
