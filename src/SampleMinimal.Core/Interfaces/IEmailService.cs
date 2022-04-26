bdbcd8d27635c484cd1ca2905d85c7063c05cab2
yun <yun@portj2.com>
2022-04-25 22:22:53 -0700

    Add email service

---
+++ src/SampleMinimal.Core/Interfaces/IEmailService.cs
@@ -0,0 +1,8 @@
+﻿namespace SampleMinimal.Core.Interfaces
+{
+    public interface IEmailService
+    {
+        Task SendEmailAsync(string toEmail, string subject, string body);
+        Task SendEmailAsync(string[] toEmail, string subject, string body);
+    }
+}
---
+++ src/SampleMinimal.Core/Services/EmailService.cs
@@ -0,0 +1,52 @@
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using MimeKit;
+
+namespace SampleMinimal.Core.Services
+{
+    public class EmailService : IEmailService
+    {
+        private IConfiguration Configuration;
+        private readonly ILogger _logger;
+        const string FROMEMAIL = "noreply@portj2.com";
+        public EmailService(IConfiguration configuration, ILogger logger)
+        {
+            Configuration = configuration;
+            _logger = logger;
+        }
+
+        public async Task SendEmailAsync(string toEmail, string subject, string emailBody)
+        {
+            string[] email = { toEmail };
+            await SendEmailAsync(email, subject, emailBody);
+        }
+
+        public async Task SendEmailAsync(string[] toEmail, string subject, string emailBody)
+        {
+            try
+            {
+                var message = new MimeMessage();
+                message.From.Add(new MailboxAddress(FROMEMAIL, FROMEMAIL));
+                foreach (var email in toEmail)
+                    message.To.Add(new MailboxAddress(email, email));
+                message.Subject = subject;
+                message.Body = new TextPart("html")
+                {
+                    Text = emailBody
+                };
+                using (var client = new SmtpClient())
+                {
+                    client.ServerCertificateValidationCallback = (s, c, h, e) => true;
+                    await client.ConnectAsync(Configuration["SMTP:Host"], Int32.Parse(Configuration["SMTP:Port"]), SecureSocketOptions.StartTls);
+                    await client.AuthenticateAsync(Configuration["SMTP:Username"], Configuration["SMTP:Password"]);
+                    await client.SendAsync(message);
+                    await client.DisconnectAsync(true);
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.Error($"Error sending email with following error: {ex}");
+            }
+        }
+    }
+}
